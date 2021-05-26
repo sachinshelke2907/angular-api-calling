a231f7df0eea3b351e7cfa38c0660bba6d1ea24c
Sachin Shelke <shelkesachin2907@gmail.com>
2021-05-26 18:46:36 +0530

    Auto refresh the list after adding new comment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,7 +12,7 @@ import { RestServiceCallerService } from './rest-service-caller.service';
 export class AppComponent implements OnInit {
     
     title = environment.title;
-    @Output() commentList : Comment[] = [];
+    commentList : Comment[] = [];
     url = 'comment.json';
 
     constructor(private restService : RestServiceCallerService) {}
@@ -21,15 +21,42 @@ export class AppComponent implements OnInit {
         this.onLoadComment();
     }
 
+    private add(comment : Comment) {
+        this.commentList.push(comment);
+    }
+
+    /**
+     * This method help use list real time as comment get added
+     * 
+     * @param rows 
+     */
+    private addCommentInList(rows : Comment[]) : void {
+        
+        this.commentList = [];
+
+        for(let row of rows){
+            this.add(row);
+        }
+    }
+
+    /**
+     * 
+     * @param comment 
+     */
     onSubmit(comment : Comment) : void {
         let result : string;
         this.restService.create(this.url, comment);
+        this.onLoadComment();
     }
 
+    /**
+     * 
+     */
     onLoadComment() : void {
 
-        this.restService.fetchAll(this.url).subscribe(posts => {
-            this.commentList = posts as Comment[];
+        this.restService.fetchAll(this.url).subscribe(comments => {
+            this.addCommentInList(comments as Comment[]);
+            // this.commentList = posts as Comment[];
             console.log(this.commentList);
         },
         error => {
